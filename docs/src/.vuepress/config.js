a06f586e1b68ed0818af179f3dbafa22a15345bd
lxx <lixinxiang@vv.cn>
2020-10-13 17:48:34 +0800

    init

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,66 @@
+const { description } = require("../../package");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "专属教程",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: "",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: false,
+    nav: [
+      {
+        text: "HTML",
+        link: "/html/",
+      },
+      {
+        text: "CSS",
+        link: "/css/",
+      },
+      {
+        text: "JS",
+        link: "/js/",
+      },
+      {
+        text: "GitHub",
+        link: "https://github.com/leer0911",
+      },
+    ],
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/plugin-medium-zoom",
+    "vuepress-plugin-auto-sidebar",
+  ],
+};
---
+++ docs/src/index.md
@@ -0,0 +1,38 @@
+---
+home: true
+heroImage: https://avatars1.githubusercontent.com/u/8563549?s=400&u=75197e8024390078914ece72fd6218e6346bd0ef&v=4
+heroText:
+tagline:
+actionText: 开始学习 →
+actionLink: /html/
+footer: Made by  with Lee
+---
+
+> 目标：打造适合国人的前端新手渐进式教程
+
+如果时间充裕的同学，可以通读 [MDN Web 入门](https://developer.mozilla.org/zh-CN/docs/Learn/Getting_started_with_the_web) 作为入门的第一步。
+
+另外，我会从自身经验角度出发精简 [MDN](https://developer.mozilla.org/zh-CN/) 学习资料，让你可以渐进式地掌握前端必备知识，形成完整的知识体系：
+
+1. 首先，你需要安装以下软件
+
+- 代码编辑器，推荐使用 [VScode](https://code.visualstudio.com/)
+- 浏览器，推荐使用 [Firefox](https://code.visualstudio.com/)、[Chrome](https://www.google.com/chrome/)
+
+2. 为了让你快速进入 Web 开发的世界，建议先阅读以下教程，并独立完成教程中的例子
+
+- [如何组织文件](https://developer.mozilla.org/zh-CN/docs/Learn/Getting_started_with_the_web/Dealing_with_files)
+- [HTML 基础](https://developer.mozilla.org/zh-CN/docs/Learn/Getting_started_with_the_web/HTML_basics)
+- [CSS 基础](https://developer.mozilla.org/zh-CN/docs/Learn/Getting_started_with_the_web/CSS_basics)
+
+3. 如果你回答以下问题还有些困难，建议你认真完成步骤 2 中的的教程
+
+- 网站应该保存在何处，使用什么结构？
+- 文件路径有哪些通用规则？
+- 什么是 HTML 标签？
+- 什么是 HTML 元素、说出你知道的元素及作用？
+- HTML 中如何引入图片？
+- HTML 中如何添加链接？
+- 怎样将文本设置为黑色或红色？
+- 怎样将内容显示在屏幕的特定位置？
+- 怎样用背景图片或颜色来装饰网页？
